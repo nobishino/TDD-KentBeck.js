6d34a6374e683bc4d352b3a1c1154d257c6e8140
nobishino <nobishino@nobishino.com>
2019-10-10 20:56:21 +0900

    p28 chap04

--- test/Part1/Money.test.js
+++ test/Part1/Money.test.js
@@ -7,10 +7,8 @@ describe("Chapter 2", () => {
     describe(`Class Dollar`, () => {
         it(`$5 * 2 = $10 副作用を排除する`, () => {
             const five = new Dollar(5);
-            let product = five.times(2);
-            assert.equal(10, product.amount);
-            product = five.times(3);
-            assert.equal(15, product.amount);
+            assert.isTrue(new Dollar(10).equals(five.times(2)));
+            assert.isTrue(new Dollar(15).equals(five.times(3)));
         });
 
         it(`equals()`, () => {
